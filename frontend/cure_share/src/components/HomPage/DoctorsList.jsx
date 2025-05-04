c91076fa125b0213192974c1b569c44737706a45
ankeshkushwaha <ankeshkush9651@gmail.com>
2025-05-04 11:00:41 +0530

    aadded the OCR repo

--- frontend/cure_share/src/components/HomPage/DoctorsList.jsx
+++ frontend/cure_share/src/components/HomPage/DoctorsList.jsx
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
 const DoctorsList = () => {
-  const doctors = ['Dr. Smith', 'Dr. Alice', 'Dr. Nikhil'];
+  const [doctors, setDoctors] = useState([]);
+  
+  const doctorId = localStorage.getItem('id');
+
+  const fetchAllProfile = async () => {
+    try {
+      const res = await axios.get('', {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token')}`
+          }
+      })
+      
+      setDoctors(res.data.allProfile);
+      toast.success(res.message)
+    }
+    catch (err) {
+      toast.error(err.message);
+    }
+  }
+  
+
+  const doctor = ['Dr. Smith', 'Dr. Alice', 'Dr. Nikhil'];
   return (
     <div className="bg-gray-200 p-4 shadow rounded-lg">
       <h3 className="font-semibold mb-2">Doctors to Follow</h3>
       <ul>
-        {doctors.map((doc, i) => (
+        {doctor.map((doc, i) => (
           <li key={i} className="flex justify-between mb-2">
             <span>{doc}</span>
             <button className="text-blue-600">Follow</button>
