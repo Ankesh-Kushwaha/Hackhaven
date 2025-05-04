9712a419f4c5d0341d11b5bd42ce2b23cf9ce176
ankeshkushwaha <ankeshkush9651@gmail.com>
2025-05-04 09:12:35 +0530

    made some change

--- frontend/cure_share/src/App.jsx
+++ frontend/cure_share/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Landing from "./components/Landing/Landing";
 import Login from "./components/Login_Sign/Login";
 import Signup from "./components/Login_Sign/Signup";
@@ -5,23 +6,15 @@ import { BrowserRouter as Router, Route ,Routes } from 'react-router-dom';
 import HomePage from "./components/HomPage/HomePage";
 import CreatePost from "./components/Post/CreatePost";
 import About from "./components/About/About";
-<<<<<<< HEAD
 import  Contact  from "./components/About/Contact";
 import Features from "./components/Landing/Features";
 import Footer from "./components/Landing/Footer";
 import Post from "./components/Post/Post";
-=======
-import Contact from "./components/Contact";
-import Features from "./components/Landing/Features";
-import Footer from "./components/Landing/Footer";
-import { ToastContainer } from "react-toastify";
-import CaseView from "./components/Post/CaseView";
->>>>>>> Amankumar140-main
 
 function App() {
   return (
     <>
-      <ToastContainer position="top-right" autoClose={3000} />
+       
       <Router>
         <Routes>
           <Route path="/" element={<Landing />} />
@@ -29,21 +22,15 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/create-case" element={<CreatePost />} />
-<<<<<<< HEAD
           <Route path="/about" element={<About/>}></Route>
           <Route path="/contact" element={<Contact/>}></Route>
           <Route path="/features" element={<Features/>}></Route>
           <Route path="/post" element={<Post/>}></Route>
 
-=======
-          <Route path="/about" element={<About />}></Route>
-          <Route path="/contact" element={<Contact />}></Route>
-          <Route path="/features" element={<Features />}></Route>
-          <Route path="/caseview/:id"  element={<CaseView/>}></Route>
->>>>>>> Amankumar140-main
         </Routes>
-        <Footer />
+        < Footer/>
       </Router>
+      
     </>
   );
 }
